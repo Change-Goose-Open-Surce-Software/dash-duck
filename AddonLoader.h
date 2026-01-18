20efa0db731788f686d016a3b5ddc000212effbd
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-01-18 11:05:06 +0100

    Delete AddonLoader.h

--- AddonLoader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-struct AddonMeta{ std::string id; std::string title; std::string developer; std::string thumbnail; };
-class AddonLoader{
-public:
-  std::vector<AddonMeta> scanAddons();
-};
